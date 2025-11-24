954c32f442bfc71af510adbebef67b225700540d
Shubham Pochhali <shubhampochhali@gmail.com>
2025-11-24 14:16:24 +0530

    complete

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 $(document).ready(function () {
   var envelope = $("#envelope");
-  var btn_open = $("#open");
-  var btn_reset = $("#reset");
 
   envelope.click(function () {
-    open();
-  });
-  btn_open.click(function () {
-    open();
-  });
-  btn_reset.click(function () {
-    close();
+    if (envelope.hasClass("open")) {
+      close();
+    } else {
+      open();
+    }
   });
 
   function open() {
